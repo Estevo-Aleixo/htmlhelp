7f28426dfd292851d293918b4beb01b42665c4b7
José Fonseca <jrfonseca@users.noreply.github.com>
2007-12-18 12:42:58 +0000

    Obey WINEPREFIX

--- misc/htmlhelp.sh
+++ misc/htmlhelp.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+WINEPREFIX=${WINEPREFIX:=$HOME/.wine}
+
+test -d "$WINEPREFIX" || wineprefixcreate
+
 # Setup the registry
 wine regedit htmlhelp.reg
 
@@ -10,9 +14,9 @@ WINEDLLOVERRIDES="advpack=n" wine htmlhelp.exe
 # Install ITSS.DLL
 cabextract -F hhupd.exe htmlhelp.exe
 cabextract -F itss.dll hhupd.exe
-cp -a itss.dll ~/.wine/drive_c/windows/system32/
+cp -a itss.dll "$WINEPREFIX/drive_c/windows/system32/"
 
 # Install MFC40.DLL
-wget http://activex.microsoft.com/controls/vc/mfc40.cab
+wget -N http://activex.microsoft.com/controls/vc/mfc40.cab
 cabextract mfc40.cab
 wine mfc40.exe
