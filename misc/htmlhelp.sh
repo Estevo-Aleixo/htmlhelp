716d244fc9e1d401d5ba237319863b5b4bdd2974
José Fonseca <jrfonseca@users.noreply.github.com>
2007-12-18 12:26:29 +0000

    New MFC40 location.

--- misc/htmlhelp.sh
+++ misc/htmlhelp.sh
@@ -8,13 +8,11 @@ wget -N 'http://go.microsoft.com/fwlink/?LinkId=14188'
 WINEDLLOVERRIDES="advpack=n" wine htmlhelp.exe
 
 # Install ITSS.DLL
-cabextract -d htmlhelp htmlhelp.exe
-cabextract -d hhupd hhupd.exe
-cabextract -d hhupd htmlhelp/hhupd.exe
-cp -a hhupd/itss.dll ~/.wine/drive_c/windows/system32/
+cabextract -F hhupd.exe htmlhelp.exe
+cabextract -F itss.dll hhupd.exe
+cp -a itss.dll ~/.wine/drive_c/windows/system32/
 
 # Install MFC40.DLL
-wget -N http://download.microsoft.com/download/sql65/Patch/6.5/WIN98/EN-US/MFC40i.exe
-cabextract -d MFC40i MFC40i.exe
-cp -a MFC40i/mfc40.dll ~/.wine/drive_c/windows/system32/
-
+wget http://activex.microsoft.com/controls/vc/mfc40.cab
+cabextract mfc40.cab
+wine mfc40.exe
